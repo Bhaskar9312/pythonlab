2cb71e536330654222f182055d9ffc68927af011
Bhaskar9312 <bhaskartejatankala006@gmail.com>
2025-04-25 22:17:25 +0530

    assignment1.py

---
+++ assignment1/assignment1.py
@@ -0,0 +1,16 @@
+int_list=[]
+print("integers are:")
+for i in range(50):
+    int_list.append(i)
+print(int_list)
+square_list=[]
+print("sqaure of that integers are:")
+for i in range(50):
+    square_list.append(i*i)
+print(square_list)
+print("alphabet list is:")
+alphabet_list=[]
+for i in range(26):
+    alphabet_list.append(chr(97+i)*(i+1))
+print(alphabet_list)
+
